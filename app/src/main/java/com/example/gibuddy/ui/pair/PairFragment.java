752281cd39b764d4aa78b9f4da7d227829afed7e
zachcacini <zcacini2@illinois.edu>
2021-04-28 17:56:25 -0500

    In Pair Fragment and ViewModel, pair button added to display a snackbar message

--- app/src/main/java/com/example/gibuddy/ui/pair/PairFragment.java
+++ app/src/main/java/com/example/gibuddy/ui/pair/PairFragment.java
@@ -1,10 +1,13 @@
 package com.example.gibuddy.ui.pair;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +16,8 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.gibuddy.R;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class PairFragment extends Fragment {
 
@@ -22,14 +27,33 @@ public class PairFragment extends Fragment {
                            ViewGroup container, Bundle savedInstanceState) {
     notificationsViewModel =
             new ViewModelProvider(this).get(PairViewModel.class);
+
+    // Sets root UI for Pair frame (from fragment_pair.xml)
     View root = inflater.inflate(R.layout.fragment_pair, container, false);
+
+    // Finds the textView in Pair frame
     final TextView textView = root.findViewById(R.id.text_pair);
+
+    // Pair Button functionality
+    final Button buttonPair = root.findViewById(R.id.buttonPair);
+    PairViewModel model = new ViewModelProvider(this).get(PairViewModel.class);
+    buttonPair.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        // Code here executes on main thread after user presses button
+        //model.setUpBluetoothAdapter();
+        Snackbar mySnackbar = Snackbar.make(root, "Please pair your GIBuddy in the Settings app", BaseTransientBottomBar.LENGTH_LONG);
+        mySnackbar.show();
+      }
+    });
+
     notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
       @Override
       public void onChanged(@Nullable String s) {
         textView.setText(s);
       }
     });
+
     return root;
+
   }
 }
--- app/src/main/java/com/example/gibuddy/ui/pair/PairViewModel.java
+++ app/src/main/java/com/example/gibuddy/ui/pair/PairViewModel.java
@@ -1,11 +1,17 @@
 package com.example.gibuddy.ui.pair;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import static androidx.core.app.ActivityCompat.startActivityForResult;
 
 public class PairViewModel extends ViewModel {
 
@@ -23,7 +29,23 @@ public class PairViewModel extends ViewModel {
     return mText;
   }
 
-  private void setUpBluetoothAdapter() {
+  public void setUpBluetoothAdapter() {
+
+    // Displays toast to open settings to pair there
+    /*
+    Context context = getApplicationContext();
+    CharSequence text = "Please open Settings to pair your GIBuddy there!";
+    int duration = Toast.LENGTH_SHORT;
+
+    Toast toast = Toast.makeText(context, text, duration);
+    toast.show();
+
+    Snackbar mySnackbar = Snackbar.make(view, stringId, duration).show();
+
+     */
+
+
+    /*
     bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     if (bluetoothAdapter == null) {
       mText.setValue("Your device doesn't support Bluetooth");
@@ -33,6 +55,13 @@ public class PairViewModel extends ViewModel {
       Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
       // line below this doesn't work
 //      startBluetoothActivity(enableBluetoothIntent, REQUEST_ENABLE_BT);
+      // modification of the line above
+        //public static int REQUEST_BLUETOOTH = 1;
+        //startActivityForResult(enableBluetoothIntent, REQUEST_BLUETOOTH);
+
     }
+
+     */
+
   }
 }
